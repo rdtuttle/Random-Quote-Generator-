50c7f404011d8866cadf7d3d12571ceb4b1fcd00
Russ Tuttle <russelltuttle1@gmail.com>
2021-11-03 14:59:10 -0600

    fixed the quotes array

--- js/script.js
+++ js/script.js
@@ -6,17 +6,24 @@ project 1 - A Random Quote Generator
 // For assistance: 
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
+  
 /*** 
  * `quotes` array 
 ***/
 const quotes = [
-  {“The story you tell yourself is largely the story you are living”, "Terrance Mckenna"},
-  {“We're all just walking each other home.”, "Ram Dass"},
-  {“Silence is a source of great strength”, "Lao Tzu"},
-  {"You are not a drop in the ocean. You are the entire ocean in a drop", "Rumi"},
-  {"Knowledge is limited", "Albert Einstein"}
-] ;
+  {quote: "The story you tell yourself is largely the story you are living",
+   source: "Terrance Mckenna"},
+  {quote: "We are all just walking eachother home.", 
+   source:"Ram Dass",
+   citation: "Be Here Now"},
+  {quote: "Silence is a source of great strength", 
+   source: "Lao Tzu",
+   year: "4th century B.C"},
+  {quote: "You are not a drop in the ocean. You are the entire ocean in a drop", 
+   source: "Rumi"},
+  {quote: "Knowledge is limited.", 
+   source: "Albert Einstein"}
+]; 
 
 
 /***
@@ -28,6 +35,7 @@ const quotes = [
 /***
  * `printQuote` function
 ***/
+
 
 
 
